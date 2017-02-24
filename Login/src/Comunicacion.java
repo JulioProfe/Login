bf2da782f0e214c79a080ed4d5862deb27df720d
JulioProfe <juliocesar15>
2017-02-24 12:47:30 -0500

    First Commit

---
+++ Login/src/Comunicacion.java
@@ -0,0 +1,11 @@
+import java.util.Observable;
+
+public class Comunicacion extends Observable implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Login/src/Main.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("Main");
+	}
+	
+	@Override
+	public void settings() {
+		// TODO Auto-generated method stub
+		size(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+
+	@Override
+	public void setup() {
+		// TODO Auto-generated method stub
+	}
+}
