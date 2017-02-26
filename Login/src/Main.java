cb44bb945e773d524f38c244ee0e8459d17cb9fd
JulioProfe <julio_15.97@hotmail.com>
2017-02-26 14:17:24 -0500

    Ji

--- Login/src/Main.java
+++ Login/src/Main.java
@@ -1,10 +1,12 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet{
+	static Logica log;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PApplet.main("Main");
+		log = new Logica();
 	}
 
 }
